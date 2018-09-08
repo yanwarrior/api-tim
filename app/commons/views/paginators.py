788dfb4d78a8fab3ff11afae28b520f86e05e4cc
Yanwar Solahudin <yanwarsolah@gmail.com>
2018-09-08 17:51:01 +0700

    Fix paginator confused

--- app/commons/views/paginators.py
+++ app/commons/views/paginators.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 def paginate(request, object_list, payload, limit=10):
     page = request.GET.get('page', 1)
     paginator = Paginator(object_list, limit)
+    next_num, prev_num = 0, 0
 
     try:
         object_list = paginator.page(page)
@@ -13,9 +14,11 @@ def paginate(request, object_list, payload, limit=10):
         object_list = paginator.page(paginator.num_pages)
 
     if object_list.has_next():
-        payload.set_state(setter=payload.SET_LINKS, next=object_list.next_page_number())
+        next_num = object_list.next_page_number()
 
     if object_list.has_previous():
-        payload.set_state(setter=payload.SET_LINKS, prev=object_list.previous_page_number())
+        prev_num = object_list.previous_page_number()
+
+    payload.set_state(setter=payload.SET_LINKS, next=next_num, prev=prev_num)
 
     return object_list, payload
